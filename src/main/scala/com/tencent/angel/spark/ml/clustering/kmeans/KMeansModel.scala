4631ea402569c6fd38a213c1e63dd6feea0972b7
yaochi <yaochi@sugo.io>
2019-09-11 18:19:23 +0800

    update kmeans implementation

--- src/main/scala/com/tencent/angel/spark/ml/clustering/kmeans/KMeansModel.scala
+++ src/main/scala/com/tencent/angel/spark/ml/clustering/kmeans/KMeansModel.scala
@@ -1,10 +1,12 @@
 package com.tencent.angel.spark.ml.clustering.kmeans
 
-import java.util
-
+import com.tencent.angel.ml.clustering.kmeans.KMeansModel._
+import com.tencent.angel.ml.core.conf.SharedConf
 import com.tencent.angel.ml.feature.LabeledData
 import com.tencent.angel.ml.math2.VFactory
 import com.tencent.angel.ml.math2.vector.{IntFloatVector, Vector}
+import com.tencent.angel.ml.matrix.{MatrixContext, RowType}
+import com.tencent.angel.spark.models.PSMatrix
 import org.apache.spark.rdd.RDD
 
 object KMeansModel {
@@ -15,26 +17,29 @@ object KMeansModel {
 
 
 class KMeansModel(ks: Array[Int]) extends Serializable {
+  private val indexRange: Long = SharedConf.indexRange
+
   private val K = ks.sum
 
-  var lcCenters: util.List[Vector] = new util.ArrayList[Vector]()
+  var lcCenters: Array[Vector] = Array()
   var lcV: IntFloatVector = VFactory.denseFloatVector(K)
   val centerDist = new Array[Double](K)
 
+  // Reference for centers matrix on PS server
+  val centers: PSMatrix = PSMatrix.matrix(new MatrixContext(KMEANS_CENTERS_MAT, K, indexRange, -1, -1, -1, SharedConf.modelType))
+  val v: PSMatrix = PSMatrix.matrix(new MatrixContext(KMEANS_CENTERS_MAT, 1, K, -1, -1, -1, RowType.T_FLOAT_DENSE))
+
+
   def train(trainData: RDD[LabeledData], param: Param): Unit = {
-    // Number of samples for each cluster
-    val spCountPerCenter = new Array[Int](K)
+    for (i <- ks.indices) {
+      val k = ks(i)
+      initKCentersRandomly(trainData, k)
 
-    initKCentersRandomly(trainData, ks(0))
-
-    for (epoch <- 1 to param.numEpoch) {
-      val startEpoch = System.currentTimeMillis()
-      trainOneEpoch(epoch, trainData, spCountPerCenter)
-      val epochTime = System.currentTimeMillis() - startEpoch
-
-      val startObj = System.currentTimeMillis()
-      val localObj = computeObjValue(trainData, epoch)
-      val objTime = System.currentTimeMillis() - startObj
+      for (epoch <- 1 to param.numEpoch) {
+        val startEpoch = System.currentTimeMillis()
+        trainOneEpoch(epoch, trainData, i, k)
+        val epochTime = System.currentTimeMillis() - startEpoch
+      }
     }
   }
 
@@ -43,6 +48,7 @@ class KMeansModel(ks: Array[Int]) extends Serializable {
     for (i <- 0 until k) {
       val newCent = points(i).getX
       newCent.setRowId(i)
+      centers.increment(newCent)
     }
   }
 
@@ -51,36 +57,40 @@ class KMeansModel(ks: Array[Int]) extends Serializable {
     * epoch from PS. Second, a mini batch of size batch_sample_num is sampled. Third, update the
     * centers in a mini-batch way.
     *
-    * @param trainData              : the trainning data storage
-    * @param per_center_step_counts : the array caches the number of samples per center
+    * @param epoch     : the epoch number
+    * @param trainData : the trainning data storage
+    * @param i         : the index of k
+    * @param k         : number of clusters
     */
-  def trainOneEpoch(epoch: Int, trainData: RDD[LabeledData], per_center_step_counts: Array[Int]): Unit = {
+  def trainOneEpoch(epoch: Int, trainData: RDD[LabeledData], i: Int, k: Int): Unit = {
+    val offset = ks.slice(0, i).sum
+    val indices = (offset to offset + k).toArray
 
-  }
+    pullCentersFromPS(indices)
+    val lcV = v.pull(0, indices)
 
-  /**
-    * Compute the objective values of all samples, which is measured by the distance from a
-    * sample to its closest center.
-    *
-    * @param data  : the trainning dataset
-    * @param epoch : the epoch number
-    */
-  def computeObjValue(data: RDD[LabeledData], epoch: Int): Double = {
-    var obj = 0.0
-    obj
+    val oldCenters = Array.ofDim[Vector](k)
+    (0 until k).foreach { i =>
+      oldCenters(i) = lcCenters(i).copy()
+    }
+
+    val oldVs = lcV.copy()
+
+    updateCenters(oldCenters, oldVs, k)
   }
 
   /**
     * Pull centers from PS to local worker
     */
-  def pullCentersFromPS(): Unit = {
+  def pullCentersFromPS(indices: Array[Int]): Unit = {
+    lcCenters = centers.pull((0 until K).toArray)
+
+    centerDist.indices.foreach { i =>
+      val c = lcCenters(i)
+      centerDist(i) = c.dot(c)
+    }
   }
 
-  /**
-    * Pull mini-batch samples from PS to local worker
-    */
-  def pullVFromPS(): Unit = {
-  }
 
   /**
     * Calculate the distance between instance and centers, and find the closest center
@@ -88,13 +98,13 @@ class KMeansModel(ks: Array[Int]) extends Serializable {
     * @param x : a instance
     * @return : the closest center id
     */
-  def findClosestCenter(x: Vector): (Int, Double) = {
+  def findClosestCenter(x: Vector, k: Int): (Int, Double) = {
     var minDis = Double.MaxValue
     var clstCent: Int = -1
 
     val len2 = x.dot(x)
     for (i <- 0 until K) {
-      val dist = centerDist(i) - 2 * lcCenters.get(i).dot(x) + len2
+      val dist = centerDist(i) - 2 * lcCenters(i).dot(x) + len2
       if (dist < minDis) {
         minDis = dist
         clstCent = i
@@ -104,7 +114,16 @@ class KMeansModel(ks: Array[Int]) extends Serializable {
   }
 
   def updateCenterDist(idx: Int): Unit = {
-    val cent: Vector = lcCenters.get(idx)
+    val cent: Vector = lcCenters(idx)
     centerDist(idx) = cent.dot(cent)
+  }
+
+  def updateCenters(oldCenters: Array[Vector], oldVs: Vector, k: Int): Unit = {
+    for (i <- 0 until k) {
+      val centerUpdate = lcCenters(i).sub(oldCenters(i))
+      centers.increment(centerUpdate)
+    }
+    val counterUpdate = lcV.sub(oldVs)
+    v.increment(counterUpdate)
   }
 }
